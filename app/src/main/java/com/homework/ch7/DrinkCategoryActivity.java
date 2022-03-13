d5fdc90809efd189c48cd565d245eae8d9efeafc
Nga Pyaw Tee <ngapyawtee02@gmail.com>
2022-03-13 11:57:58 +0630

    Cursor done

--- app/src/main/java/com/homework/ch7/DrinkCategoryActivity.java
+++ app/src/main/java/com/homework/ch7/DrinkCategoryActivity.java
@@ -2,23 +2,48 @@ package com.homework.ch7;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 
 public class DrinkCategoryActivity extends Activity {
+    private Cursor cursor;
+    private SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitiy_drinks_category);
 
-        ArrayAdapter<Drink> arrayAdapter = new ArrayAdapter<>(DrinkCategoryActivity.this, android.R.layout.simple_list_item_1, Drink.drinks);
-
         ListView listView = (ListView) findViewById(R.id.drink_cate_list_view);
-        listView.setAdapter(arrayAdapter);
+
+        SQLiteOpenHelper helper = new Ch7DatabaseHelper(this);
+        try{
+            db = helper.getReadableDatabase();
+            cursor = db.query("CH7",
+                    new String[]{"_id","NAME"},
+                    null,null,null,null,null);
+
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1});
+            listView.setAdapter(adapter);
+
+        }catch (SQLiteException e){
+            Toast.makeText(this, "Database Unavaliable", Toast.LENGTH_SHORT).show();
+        }
+
 
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
@@ -29,5 +54,12 @@ public class DrinkCategoryActivity extends Activity {
             }
         };
         listView.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
--- app/src/main/java/com/homework/ch7/DrinkDetailActivity.java
+++ app/src/main/java/com/homework/ch7/DrinkDetailActivity.java
@@ -3,9 +3,14 @@ package com.homework.ch7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkDetailActivity extends AppCompatActivity {
 
@@ -17,14 +22,33 @@ public class DrinkDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drink_detail);
 
         int drinkID = (int) getIntent().getExtras().get(EXTRA_ID);
-        Drink drink = Drink.drinks[drinkID];
 
         ImageView imageView = findViewById(R.id.image_view);
         TextView textView1 = findViewById(R.id.text_view1);
         TextView textView2 = findViewById(R.id.text_view2);
 
-        imageView.setImageResource(drink.getImageResourceID());
-        textView1.setText(drink.getName());
-        textView2.setText(drink.getDescription());
+        SQLiteOpenHelper helper =new Ch7DatabaseHelper(this);
+        try {
+            SQLiteDatabase db = helper.getReadableDatabase();
+            Cursor cursor = db.query("CH7",
+                    new String[]{"NAME","DESCRIPTION","IMAGEID"},
+                    "_id=?",
+                    new String[]{Integer.toString(drinkID)},
+                    null,null,null);
+
+            if (cursor.moveToFirst()){
+                String nameText = cursor.getString(0);
+                String descText = cursor.getString(1);
+                int imgID = cursor.getInt(2);
+
+                textView1.setText(nameText);
+                textView2.setText(descText);
+                imageView.setImageResource(imgID);
+            }
+            cursor.close();
+            db.close();
+        }catch (SQLiteException e){
+            Toast.makeText(this, "Database Unavaliable", Toast.LENGTH_SHORT).show();
+        }
     }
 }
